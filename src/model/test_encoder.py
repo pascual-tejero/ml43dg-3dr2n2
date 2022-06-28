e9b932bd710a8cb06ea08846a77fdc11cbe7856b
barandenizkorkmaz <deniz.korkmaz@boun.edu.tr>
2022-06-28 19:38:31 +0200

    Add test scripts for encoder and decoder.

--- src/model/test_encoder.py
+++ src/model/test_encoder.py
@@ -1,17 +1,17 @@
 import torch
 
-from decoder import Decoder
+from encoder import Encoder
 
 # The input to the decoder is the hidden state of the 3DConvLSTM,
 # a tensor of size (B, C, D, H, W), where (D, H, W) are the 3D LSTM grid,
 # (4, 4, 4) in the paper, C is the hidden state size at each point in the grid
 # (128 according to the original code), and B is the batch size.
 
-model = Decoder(type="simple")
+model = Encoder(type="simple")
 
-B, C, D, H, W = (1, 128, 4, 4, 4)
+B, C, H, W = (1, 3, 127, 127)
 
-x = torch.rand(B,C,D,H,W)
+x = torch.rand(B,C,H,W)
 
 out = model(x)
 print(out.shape)
