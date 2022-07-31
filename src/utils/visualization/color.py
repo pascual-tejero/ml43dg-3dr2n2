bb7aee1efef82459b693c2369b773516a54b6ec0
ewriji <ewriji@airlab.team>
2022-07-31 18:36:44 +0200

    feat: Utils - Add grid creation method

--- src/utils/visualization/color.py
+++ src/utils/visualization/color.py
@@ -8,7 +8,7 @@ def visualize_colored_points(
     x,
     y,
     z,
-    sdf_values,
+    opacity_values,
     color_values,
     *,
     distance=0.01,
@@ -24,7 +24,7 @@ def visualize_colored_points(
     @param x: X coordinate of a grid
     @param y: Y coordinate of a grid
     @param z: Z coordinate of a grid
-    @param sdf_values: Values of signed distance function evaluated at (x,y,z) point
+    @param opacity_values: Values of opacity assigned to poitns
     @param color_values: Color values at (x,y,z) point
     @param distance: Threshold value.
                     All drawn point will have  -distance < sdf < distance
@@ -34,29 +34,23 @@ def visualize_colored_points(
     @return go.Figure instance
     """
 
-    sdf_values = sdf_values.flatten()
+    opacity_values = opacity_values.flatten()
     color_values = color_values.reshape(-1, 3)
-
-    close_points_mask = (sdf_values <= distance) & (sdf_values >= -distance)
-
-    x = x[close_points_mask]
-    y = y[close_points_mask]
-    z = z[close_points_mask]
-    close_points_colors = color_values[close_points_mask]
 
     sub_idx = np.random.choice(np.arange(x.shape[0]), size=sample_size)
 
     x = x[sub_idx]
     y = y[sub_idx]
     z = z[sub_idx]
-    close_points_colors = close_points_colors[sub_idx]
+    close_points_colors = color_values[sub_idx]
+    opacity_values = opacity_values[sub_idx]
 
     scatter = go.Scatter3d(
         x=x,
         y=y,
         z=z,
         mode="markers",
-        marker=dict(size=2, color=close_points_colors, opacity=1),
+        marker=dict(size=2, color=close_points_colors, opacity=opacity_values),
     )
 
     if return_scatter:
